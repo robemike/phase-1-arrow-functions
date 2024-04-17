c3275514116ceb04a6052922024e744ffa1d5690
robemike <robemike1135@gmail.com>
2024-04-17 10:49:31 +0300

    completes tests

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function (){
+    let a = 2000
+    let b = 100
+    return a/b
+};
+const square = num => num * num;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
